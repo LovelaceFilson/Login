c843145129d2600fdc3906c07faeb3bff6f47ba5
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2020-09-27 16:34:37 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+  FlatList
+} from 'react-native';
+import LoginScreen from './LoginScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      names: [
+        "ama",
+        "kofi",
+        "sam",
+        "bob",
+        "james",
+        "ama",
+        "kofi",
+        "sam",
+        "bob",
+      ],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <LoginScreen/>
+       
+
+
+
+        {/* <TextInput 
+        style={styles.input}
+        placeholder="email"
+        autoCapitalize
+        autoCorrect={false}
+        //secureTextEntry={true}
+        value={this.state.email}
+        onChangeText={(email)=>{
+          this.setState({email})
+        }}
+        />
+
+<TextInput 
+        style={styles.input}
+        placeholder="password"
+        autoCapitalize
+        autoCorrect={false}
+        secureTextEntry={true}
+        value={this.state.password}
+        onChangeText={(password)=>{
+          this.setState({password})
+        }}
+        />
+
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.button}>Login</Text>
+        </TouchableOpacity> */}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+  },
+  input: {
+    backgroundColor: "green",
+    height: 60,
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  button: {
+    color: "white",
+    fontSize: 20,
+  },
+  buttonContainer: {
+    marginHorizontal: 20,
+    backgroundColor: "blue",
+    height: 35,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
   },
 });
+
+export default App;
---
+++ LoginScreen.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.loginTextContainer}>
+          <Text style={styles.loginText}>Log in</Text>
+        </View>
+
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor="#aaaaaa"
+            placeholder="Username"
+          />
+          <TextInput
+            style={styles.input}
+            placeholderTextColor="#aaaaaa"
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+          <Text style={styles.forgotPassword}>Forgot password</Text>
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.noAccountContainer}>
+          <Text style={styles.noAccountText}>Don't have an account?</Text>
+          <Text style={styles.signupText}>Sign up</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 50,
+  },
+  loginText: {
+    fontSize: 50,
+    color: "#5100ad",
+  },
+  loginTextContainer: {
+    marginBottom: 30,
+  },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#5100ad",
+    fontSize: 20,
+    height: 50,
+    marginTop: 20
+    
+  },
+  forgotPassword:{
+      alignSelf:"flex-end",
+      marginVertical: 3,
+      color: "#0853a8",
+  },
+  buttonContainer:{
+      height: 50,
+      backgroundColor: "#5100ad",
+      justifyContent:"center",
+      alignItems: "center",
+      borderRadius: 10,
+      marginVertical: 50,
+  },
+  buttonText:{
+      color:"white",
+      fontSize: 25,
+     
+  },
+  noAccountContainer:{
+      flexDirection: "row",
+      justifyContent:"center"
+  },
+  noAccountText:{
+      marginRight: 10,
+      fontSize:16 
+  },
+  signupText:{
+      fontSize: 16,
+      color: "#5100ad"
+  }
+  });
